381d471fa888a0aee96abf29629e054b07ddd214
Bertrik Sikken <bertrik@sikken.nl>
2017-10-22 22:54:03 +0200

    Update PWM frequency to 70 kHz.

--- current.cpp
+++ current.cpp
@@ -10,7 +10,7 @@ void CurrentInit(void)
     pinMode(PIN_CURRENT_OUT, PWM);
 
     pwmtimer.setPrescaleFactor(1);
-    pwmtimer.setOverflow(4096);       // Instead of 2^16 (72MHz/2^16=1100Hz) it overflows at 2^12 (4096, 17kHz)
+    pwmtimer.setOverflow(1023);
 }
 
 float CurrentSetValue(float current)
@@ -24,7 +24,7 @@ float CurrentSetValue(float current)
         setting = current;
     }
 
-    int pwm = (int)(4095 * setting / 5.0);
+    int pwm = (int)(1024 * setting / 5.0);
     pwmWrite(PIN_CURRENT_OUT, pwm);
 
     return setting;
