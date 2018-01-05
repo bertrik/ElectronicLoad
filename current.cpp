92a6d58fb80e212b76e9551ca07940ca1685de51
Jasper Sikken <jasper@sodaq.com>
2018-01-05 21:46:18 +0100

    Fixed the problem that load current would not go below 6mA

--- current.cpp
+++ current.cpp
@@ -8,15 +8,17 @@ static float setting = 0.0;
 void CurrentInit(void)
 {
     pinMode(PIN_CURRENT_OUT, PWM);
-
+    // following two lines set the PWM to 70 kHz
     pwmtimer.setPrescaleFactor(1);
-    pwmtimer.setOverflow(1023);
+    pwmtimer.setOverflow(1023); // counts to 1023 and then overflows
+    // 72MHz clock / 1024 = 70.312 kHz
+    // current set resolution is 1024 levels/10bits
 }
 
 float CurrentSetValue(float current)
 {
     // limit current to range
-    if (current < 0.0) {
+    if (current < 0.005) {
         setting = 0.0;
     } else if (current > 5.0) {
         setting = 5.0;
@@ -25,7 +27,16 @@ float CurrentSetValue(float current)
     }
 
     int pwm = (int)(1024 * setting / 5.0);
-    pwmWrite(PIN_CURRENT_OUT, pwm);
+
+    // MCU output does not go below ~4mV, causing ~6mA load current. 
+    if (setting<0.005){ // for currents below 5mA
+      // The pin is made input, the voltage divider pulls the voltage to 0mV and load current is 0mA.
+      pinMode(PIN_CURRENT_OUT, INPUT);
+    }
+    else{
+      pinMode(PIN_CURRENT_OUT, PWM);
+      pwmWrite(PIN_CURRENT_OUT, pwm);
+    }
 
     return setting;
 }
